d39d3269245c30cff1cea91a2841033adc0af2b8
P4 <umeaansa555@ed.amdsb.ca>
2021-10-14 14:17:29 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace GuessingGamePractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number between 1 and 100";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(121, 103);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(140, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(101, 190);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(82, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "Enter a number.";
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(164, 129);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(52, 38);
+            this.enterButton.TabIndex = 3;
+            this.enterButton.Text = "Guess";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 254);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button enterButton;
+    }
+}
+
